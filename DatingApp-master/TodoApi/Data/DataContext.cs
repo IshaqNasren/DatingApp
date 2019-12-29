52dd12b3ec5adeedd20d99f30ba3fc615e1d406a
unknown <ishaqnasren1999@gmail.com>
2019-12-29 15:17:48 +0200

    mywork

---
+++ DatingApp-master/TodoApi/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Model;
+
+namespace TodoApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set;}
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
